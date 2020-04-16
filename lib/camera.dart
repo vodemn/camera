0ea8997355dbbd015892fbfe1105609ec9be1669
Vadim <44135514+vodemn@users.noreply.github.com>
2020-04-16 12:46:27 +0300

    double

--- lib/camera.dart
+++ lib/camera.dart
@@ -594,7 +594,7 @@ class CameraController extends ValueNotifier<CameraValue> {
   /// change zoom by specific [step].
   /// with a negative step, the zoom will be 1
   ///
-  Future<void> zoom(int step) async {
+  Future<void> zoom(double step) async {
     await _channel.invokeMethod<void>('zoom', <String, dynamic>{'step': step});
   }
 
